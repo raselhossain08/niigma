181b9d50cddb20b1c599d26821ed752919b7fceb
Rasel Hossain <61477379+raselhossain08@users.noreply.github.com>
2025-07-20 05:38:37 +0600

    new

--- src/types/CarouselItem.ts
+++ src/types/CarouselItem.ts
@@ -206,7 +206,7 @@ export const carouselItemData: CarouselItemData[] = [
       h: 611,
     },
     topLeftImage: {
-      src: '/img/hero/slide/7.svg',
+      src: '/img/hero/slide/7.png',
       width: 221.61,
       height: 78,
       alt: '',
@@ -251,7 +251,7 @@ export const carouselItemData: CarouselItemData[] = [
       h: 611,
     },
     topLeftImage: {
-      src: '/img/hero/slide/9.svg',
+      src: '/img/hero/slide/8.png',
       width: 221.61,
       height: 78,
       alt: '',
